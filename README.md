8468f8175299b19daaaebfef1d16d745dcb68150
Taylor Merwin <taylor.merwin@gmail.com>
2025-04-15 12:58:44 -0700

    Added schema.sql

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Bluetrends
 Capstone project for Dev10 Data Engineering 2025-3 Cohort
+
+
+# Steps to run:
+1. Start up docker - `docker compose up -d` or `docker compose up --build -d`
+
+2. Start the producer ` docker compose exec python python jetstream_producer.py`
+
+3. Start spark consumer `docker exec -it spark-master spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.5 /opt/spark-apps/jobs/jetstream_consumer.py`
+
+I have no name!@5622bbdb1314:/opt/spark-apps/jobs$ spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.5 jetstream_consumer.py
---
+++ mysql/initdb/schema.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE IF NOT EXISTS bluetrends;
+USE bluetrends;
+
+
+CREATE TABLE IF NOT EXISTS categories (
+    category_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(100) UNIQUE NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS keywords (
+    keyword_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    keyword_name VARCHAR(100) UNIQUE NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS posts (
+    post_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    did VARCHAR(255) NOT NULL,
+    text TEXT NOT NULL,
+    created_at DATETIME NOT NULL,
+    language VARCHAR(10),
+    sfw BOOLEAN DEFAULT TRUE,
+    sentiment_score FLOAT,
+    sentiment_label VARCHAR(50),
+    category_id BIGINT,
+    CONSTRAINT fk_category_id
+        FOREIGN KEY (category_id)
+        REFERENCES categories(category_id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
+
+);
+
+CREATE TABLE IF NOT EXISTS post_keywords (
+      post_id BIGINT NOT NULL,
+  keyword_id BIGINT NOT NULL,
+  PRIMARY KEY (post_id, keyword_id),
+  CONSTRAINT fk_post_id
+    FOREIGN KEY (post_id)
+    REFERENCES posts(post_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT fk_keyword_id
+    FOREIGN KEY (keyword_id)
+    REFERENCES keywords(keyword_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
